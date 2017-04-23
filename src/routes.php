16316dcb46b6168d6ac21189f1dfb35b7b385a22
Kai Sassnowski <ksassnowski@gmail.com>
2017-04-23 21:56:27 +0200

    Remove route group from routes

--- src/routes.php
+++ src/routes.php
@@ -1,26 +1,24 @@
 <?php
 
-Route::group(['middleware' => 'web'], function () {
-    Route::get('beanstalkd/tubes', [
-        'as' => 'beanstalkd.tubes.index',
-        'uses' => 'Dionera\BeanstalkdUI\Controllers\TubesController@index',
-    ]);
+Route::get('beanstalkd/tubes', [
+    'as' => 'beanstalkd.tubes.index',
+    'uses' => 'Dionera\BeanstalkdUI\Controllers\TubesController@index',
+]);
 
-    Route::get('beanstalkd/tubes/{tube}', [
-        'as' => 'beanstalkd.tubes.show',
-        'uses' => 'Dionera\BeanstalkdUI\Controllers\TubesController@showTube',
-    ]);
+Route::get('beanstalkd/tubes/{tube}', [
+    'as' => 'beanstalkd.tubes.show',
+    'uses' => 'Dionera\BeanstalkdUI\Controllers\TubesController@showTube',
+]);
 
-    Route::delete('beanstalkd/jobs/{job}', [
-        'as' => 'beanstalkd.jobs.delete',
-        'uses' => 'Dionera\BeanstalkdUI\Controllers\JobsController@delete',
-    ]);
+Route::delete('beanstalkd/jobs/{job}', [
+    'as' => 'beanstalkd.jobs.delete',
+    'uses' => 'Dionera\BeanstalkdUI\Controllers\JobsController@delete',
+]);
 
-    Route::post('beanstalkd/jobs/{job}', [
-        'as' => 'beanstalkd.jobs.kick',
-        'uses' => 'Dionera\BeanstalkdUI\Controllers\JobsController@kick',
-    ]);
-});
+Route::post('beanstalkd/jobs/{job}', [
+    'as' => 'beanstalkd.jobs.kick',
+    'uses' => 'Dionera\BeanstalkdUI\Controllers\JobsController@kick',
+]);
 
 Route::get('api/tube/{tube}', [
     'as' => 'beanstalkd.stats',
